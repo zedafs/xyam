d8ad7899c09bfcaeb40192431ff322c8117ffa01
zedafs <138558107+zedafs@users.noreply.github.com>
2024-02-17 14:40:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function updateDateTime() {
     const dateTimeElement = document.getElementById('datetime');
     const currentDate = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
     const formattedDate = currentDate.toLocaleDateString('en-US', options);
     dateTimeElement.textContent = formattedDate;
 }
